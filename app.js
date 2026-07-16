e5caa8e9a3f1b37f4b96b30e7555c7b158e912a4
Sufiyan Shahid <sufiyanshahid144@gmail.com>
2026-07-16 10:23:12 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,4 +1,120 @@
 
+// Section 1 – Objects (1–5)
+
+// Q1.
+// JavaScript Object kya hota hai? Real-world example ke sath explain karein.
+
+
+// Ans: A JavaScript Object is a collection of related data stored as key-value pairs.
+
+// Real world example:
+
+// const student = {
+//     name: "Ali",
+//     age: 20,
+//     city: "Karachi"
+// };
+
+// console.log(student);
+
+
+
+// Q2.
+// Dot notation aur Bracket notation me kya difference hai?
+
+// Dot notation is Simple and easy to read.
+
+// Bracket notation is Useful for dynamic property names or property names with spaces.
+
+
+
+// const user = {
+
+//     name: "Hasan",
+
+//     age: 22
+
+// };
+
+// // Dot Notation
+// console.log(user.name);
+
+// // Bracket Notation
+// console.log(user["name"]);
+
+// Dono tarikon se name access karke dikhayein.
+
+
+// Q3.
+// Niche diye object ka output batayein.
+
+
+
+// const student = {
+
+// name: "Ali",
+
+// age: 20
+
+// };
+
+
+
+// student.city = "Karachi";
+
+// delete student.age;
+
+
+
+// console.log(student); // {  name: "Ali",  city: "Karachi"}
+
+
+// Q4.
+// Object.keys(), Object.values(), aur Object.entries() kya return karte hain?
+
+// Example ke sath explain karein.
+
+// Object.keys(): Returns all property names.
+// Object.values(): Returns all property values.
+// Object.entries(): Returns key-value pairs as nested arrays.
+
+// const student = {
+//     name: "Ali",
+//     age: 20,
+//     city: "Karachi"
+// };
+
+// console.log(Object.keys(student));
+// console.log(Object.values(student));
+// console.log(Object.entries(student));
+
+
+// Q5.
+// Ek function likhein jo object ke andar total properties count kare.
+
+// Expected:
+
+// function countProperties(obj) {
+//     return Object.keys(obj).length;
+// }
+
+// console.log(
+//     countProperties({
+//         name: "Ali",
+//         age: 20,
+//         city: "Lahore"
+//     })
+// );
+
+
+// Section 2 – Arrays (6–10)
+
+// Q6.
+// Array aur Object me kya difference hai?
+
+// Ans: Array Stores multiple values. It Uses index numbers. It can be Written using [].
+// Object Stores key-value pairs. It Uses property names (keys). It can be Written using {}.
+
 // Section 7 – call(), apply(), bind() (27–29)
 
 // Q27.
@@ -37,7 +153,8 @@
 
 
 // Q29.
-// Output batayein.
+// Output batayein.
+
 
 
 // const user = {
